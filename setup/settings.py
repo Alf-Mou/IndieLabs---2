b993414613ac0c6529ef1a0ed274bfb3b93b92b1
Alfredo Moura <alfred.mou99@gmail.com>
2024-06-12 16:54:13 -0300

    Fixed .gitignore issues

--- setup/settings.py
+++ setup/settings.py
@@ -109,9 +109,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'setup/static')
+]
+
 
 # Media
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
