6f41e5fe9cf69b2560bff20ab116c7fe55bcbcaf
Alf-mou <alfred.mou99@gmail.com>
2024-06-22 07:51:24 -0300

    Modified Models

--- game/models.py
+++ game/models.py
@@ -6,7 +6,7 @@ class Game(models.Model):
     mini_descricao = models.TextField(max_length=400)
     imagem_destaque = models.ImageField(upload_to="fotos/", blank=True, null=True)
     imagem_miniatura = models.ImageField(upload_to="fotos/")
-    descricao_completa = models.TextField(max_length=2000)
+    descricao_completa = models.TextField(max_length=2000,  blank=True, null=True)
     imagem_gameplay1 = models.ImageField(upload_to="fotos/")
     imagem_gameplay2 = models.ImageField(upload_to="fotos/")
     imagem_gameplay3 = models.ImageField(upload_to="fotos/")
