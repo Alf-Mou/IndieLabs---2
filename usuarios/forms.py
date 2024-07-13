8d80667c0bb24bb01310551ef04035362e448fae
Alfredo Moura <alfred.mou99@gmail.com>
2024-07-13 20:56:17 -0300

    Refactored Forms: Migrated to Django Forms and Implemented Loop for Fields in HTML

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,14 +1,77 @@
 from django import forms
 
 class LoginForms(forms.Form):
-    nome_login=forms.CharField(
-        label="Nome de Login",
+    email=forms.EmailField(
+        label="Email",
         required=True,
-        max_length=100
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form__input",
+                "placeholder": "Email"
+            }
+        )
     )
+
     senha=forms.CharField(
         label="Senha",
         required=True,
         max_length=70,
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form__input",
+                "placeholder": "Senha"
+            }
+        )
+    )
+
+class CadastroForms(forms.Form):
+    nome_completo=forms.CharField(
+        label="Nome",
+        required=True,
+        max_length=100,
+                widget=forms.TextInput(
+            attrs={
+                "class": "form__input",
+                "placeholder": "Nome completo"
+            }
+        )
+    )
+
+
+    pais = forms.ChoiceField(
+        label="País",
+        required=True,
+        choices=[('', 'Selecione um país')],  
+        widget=forms.Select(
+            attrs={
+                "id": "country",
+                "class": "form__select",
+                "placeholder": "Selecione um país"
+            }
+        )
+    )
+
+    email=forms.EmailField(
+        label="Email",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form__input",
+                "placeholder": "Email"
+            }
+        )
+    )
+
+    senha=forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form__input",
+                "placeholder": "Senha"
+            }
+        )
     )
